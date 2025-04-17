c73fcb17ac6e22f94630d4fc0532f3864d49d72e
N02743 <Utimatentch@gmail.com>
2025-04-17 21:31:12 +0700

    refactor graph canvas.py

--- Canvas.py
+++ Canvas.py
@@ -39,26 +39,25 @@ class CanvasGraph(tk.Canvas):
         super().__init__(parent, bg="white")
         self.pack()
 
-        self.setVariableFromFile()
-        self.setVariableFromParameter(date_range, lab_results, medicine_usage)
+        self.redraw(
+            date_range=date_range,
+            lab_results=lab_results,
+            medicine_usage=medicine_usage,
+        )
 
-        self.reconfighWidthAndHeight()
+    # return x position at center for row = 0 at day = index
+    def day_x(self, index):
+        return self.label_width + index * self.day_width + self.day_width // 2
 
-        # TODO: expand size calculate
+    # return y position for element in graph at row = row_idx
+    def row_y(self):
+        return self.canvas_padding + self.row_idx * self.row_height
 
-        row_idx = 0
-
-        # Image row
-        row_idx = self.createImageButton(row_idx)
-
-        # Lab results
-        row_idx = self.drawLabResults(lab_results, row_idx)
-
-        # Day Range
-        row_idx = self.drawDayRange(row_idx)
-
-        # Medicine
-        self.DrawMedicine(medicine_usage, row_idx)
+    def setVariableFromFile(self):
+        self.label_width = Var.graphLabel_width
+        self.day_width = Var.graphDay_width
+        self.row_height = Var.graphRow_height
+        self.canvas_padding = Var.graphCanvas_padding
 
     def setVariableFromParameter(self, date_range, lab_results, medicine_usage):
         self.date_range = date_range
@@ -69,15 +68,73 @@ class CanvasGraph(tk.Canvas):
         medicine_usage_rows = len(self.medicine_usage)
         self.total_rows = 1 + lab_test_rows + 1 + medicine_usage_rows
 
-    def setVariableFromFile(self):
-        self.label_width = Var.graphLabel_width
-        self.day_width = Var.graphDay_width
-        self.row_height = Var.graphRow_height
-        self.canvas_padding = Var.graphCanvas_padding
+    def reconfighWidthAndHeight(self):
+        self.recalWidthAndHeight()
+        self.config(
+            width=self.width,
+            height=self.height,
+        )
 
-    def DrawMedicine(self, medicine_usage, row_idx):
-        for med in medicine_usage:
-            y = self.row_y(row_idx)
+    def recalWidthAndHeight(self):
+        self.width = (
+            self.label_width
+            + (len(self.date_range) * self.day_width)
+            + (self.canvas_padding * 2)
+        )
+
+        self.height = (self.total_rows * self.row_height) + (self.canvas_padding * 2)
+
+    def createImageButton(self):
+        for i in range(len(self.date_range)):
+            x = self.day_x(i)
+            y = self.row_y()
+            btn = tk.Button(
+                self,
+                text="📷",
+                command=lambda d=i: showImageModal(d),
+                width=2,
+            )
+            self.create_window(x, y + 25, window=btn)
+        self.row_idx += 1
+
+    def drawLabResults(self):
+        for test, values in self.lab_results.items():
+            y = self.row_y()
+            self.create_text(
+                5,
+                y + 10,
+                anchor="w",
+                text=test,
+                font=Var.graph_font,
+            )
+            for i, val in enumerate(values):
+                if val is not None:
+                    x = self.day_x(i)
+                    self.create_text(
+                        x,
+                        y + 10,
+                        text=str(val),
+                        font=Var.graph_font,
+                    )
+            self.row_idx += 1
+
+    def drawDayRange(self):
+        y = self.row_y()
+        self.create_text(
+            5,
+            y + 10,
+            anchor="w",
+            text="Day",
+            font=Var.graph_font,
+        )
+        for i in range(len(self.date_range)):
+            x = self.day_x(i)
+            self.create_text(x, y + 10, text=str(i + 1))
+        self.row_idx += 1
+
+    def DrawMedicine(self):
+        for med in self.medicine_usage:
+            y = self.row_y()
             self.create_text(
                 5,
                 y + 10,
@@ -88,107 +145,51 @@ class CanvasGraph(tk.Canvas):
 
             start_idx = med["start"] - 1
             end_idx = med["end"] - 1
-            x1 = self.day_x(start_idx) + self.day_width // 2
-            x2 = self.day_x(end_idx) + self.day_width // 2
+            x1 = self.day_x(start_idx)
+            x2 = self.day_x(end_idx)
             timelinePositionY = y + 10
 
             self.drawTimeline(x1, x2, timelinePositionY)
 
-            row_idx += 1
+            self.row_idx += 1
 
-    def drawTimeline(self, x1, x2, timelinePositionY):
+    def drawTimeline(self, x1, x2, timelinePosY):
         self.create_line(
-            x1, timelinePositionY, x2, timelinePositionY, fill="blue", width=6
+            x1,
+            timelinePosY,
+            x2,
+            timelinePosY,
+            fill="blue",
+            width=6,
         )
         self.create_oval(
-            x1 - 5, timelinePositionY - 5, x1 + 5, timelinePositionY + 5, fill="green"
+            x1 - 5,
+            timelinePosY - 5,
+            x1 + 5,
+            timelinePosY + 5,
+            fill="green",
         )
         self.create_oval(
-            x2 - 5, timelinePositionY - 5, x2 + 5, timelinePositionY + 5, fill="red"
+            x2 - 5,
+            timelinePosY - 5,
+            x2 + 5,
+            timelinePosY + 5,
+            fill="red",
         )
 
-    def drawDayRange(self, row_idx):
-        y = self.row_y(row_idx)
-        self.create_text(
-            5,
-            y + 10,
-            anchor="w",
-            text="Day",
-            font=Var.graph_font,
-        )
-        for i in range(len(self.date_range)):
-            x = self.day_x(i) + self.day_width // 2
-            self.create_text(x, y + 10, text=str(i + 1))
-        row_idx += 1
-        return row_idx
-
-    def drawLabResults(self, lab_results, row_idx):
-        for test, values in lab_results.items():
-            y = self.row_y(row_idx)
-            self.create_text(
-                5,
-                y + 10,
-                anchor="w",
-                text=test,
-                font=Var.graph_font,
-            )
-            for i, val in enumerate(values):
-                if val is not None:
-                    x = self.day_x(i) + self.day_width // 2
-                    self.create_text(
-                        x,
-                        y + 10,
-                        text=str(val),
-                        font=Var.graph_font,
-                    )
-            row_idx += 1
-        return row_idx
-
-    def createImageButton(self, row_idx):
-        for i in range(len(self.date_range)):
-            x = self.day_x(i) + self.day_width // 2
-            y = self.row_y(row_idx)
-            btn = tk.Button(
-                self,
-                text="📷",
-                command=lambda d=i: showImageModal(d),
-                width=2,
-            )
-            self.create_window(x, y + 25, window=btn)
-        row_idx += 1
-
-        return row_idx
-
-    def day_x(self, index):
-        return self.label_width + index * self.day_width
-
-    def row_y(self, index):
-        return self.canvas_padding + index * self.row_height
-
-    def recalWidthAndHeight(self):
-        # TODO: Calculate width and height
-        self.width = (
-            self.label_width
-            + (len(self.date_range) * self.day_width)
-            + (self.canvas_padding * 2)
-        )
-        self.height = (self.total_rows * self.row_height) + (self.canvas_padding * 2)
-
-    def reconfighWidthAndHeight(self):
-        self.recalWidthAndHeight()
-        self.config(
-            width=self.width,
-            height=self.height,
-        )
-
-    def redraw(self):
+    def redraw(self, date_range, lab_results, medicine_usage):
         self.delete("all")
+
+        self.setVariableFromFile()
+        self.setVariableFromParameter(date_range, lab_results, medicine_usage)
+
         self.reconfighWidthAndHeight()
 
-        # TODO: Image Button
+        # TODO: expand size calculate
 
-        # TODO: Lab test
+        self.row_idx = 0
 
-        # TODO: Day Range Line
-
-        # TODO: Medicine Usage
+        self.createImageButton()
+        self.drawLabResults()
+        self.drawDayRange()
+        self.DrawMedicine()
